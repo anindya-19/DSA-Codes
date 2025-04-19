909f6d2fcc6bcbb5359444a9dcb2106945bf8f17
anindya-19 <anindyaganguly19@gmail.com>
2025-04-19 22:11:28 +0530

    graham scan convex hull

--- grahamScanConvexHull.c
+++ grahamScanConvexHull.c
@@ -7,12 +7,27 @@ typedef struct point
 {
     int x, y;
 } Point;
-
 Point stack[MAX];
 int top = -1;
+// A global point needed for  sorting points with reference
+// to  the first point Used in compare function of qsort()
+Point p0;
 void convexHull(Point points[], int n)
 {
     int ymin = points[0].y, min = 0;
+    for (int i = 1; i < n; i++)
+    {
+        // Pick the bottom-most or choose the left
+        // most point in case of tie
+        int y = points[i].y;
+        if ((y < ymin) || (y == ymin && points[i].x < points[min].x))
+        {
+            ymin = points[i].y;
+            min = i;
+        }
+    }
+
+    swap(&points[0], &points[min]);
 }
 int main()
 {
