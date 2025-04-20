e3626782f66bda5e001dc3e32ac3487fb6f50862
anindya-19 <anindyaganguly19@gmail.com>
2025-04-20 20:54:58 +0530

    code completion

--- grahamScanConvexHull.c
+++ grahamScanConvexHull.c
@@ -12,6 +12,54 @@ int top = -1;
 // A global point needed for  sorting points with reference
 // to  the first point Used in compare function of qsort()
 Point p0;
+
+void swap(Point *a, Point *b)
+{
+    Point temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+int orientation(Point p, Point q, Point r)
+{
+    int val = (q.y - p.y) * (r.x - q.x) - (q.x - p.x) * (r.y - q.y);
+    if (val == 0)
+        return 0;             // collinear
+    return (val > 0) ? 1 : 2; // 1: clockwise, 2: counter-clockwise
+}
+
+int distSq(Point p1, Point p2)
+{
+    return (p1.x - p2.x) * (p1.x - p2.x) + (p1.y - p2.y) * (p1.y - p2.y); //using pythagorean theorem to find the distance between two points
+}
+
+int compare(const void *a, const void *b)
+{
+    Point *p1 = (Point *)a;
+    Point *p2 = (Point *)b;
+
+    int o = orientation(p0, *p1, *p2);
+    if (o == 0)
+        return (distSq(p0, *p2) >= distSq(p0, *p1)) ? -1 : 1;
+
+    return (o == 2) ? -1 : 1;
+}
+
+void push(Point p)
+{
+    stack[++top] = p;
+}
+
+Point nextToTop()
+{
+    return stack[top - 1];
+}
+
+Point pop()
+{
+    return stack[top--];
+}
+
 void convexHull(Point points[], int n)
 {
     int ymin = points[0].y, min = 0;
@@ -28,6 +76,26 @@ void convexHull(Point points[], int n)
     }
 
     swap(&points[0], &points[min]);
+    p0 = points[0];
+
+    qsort(&points[1], n - 1, sizeof(Point), compare);
+
+    // Create an empty stack and push first three points
+    // to it.
+    push(points[0]);
+    push(points[1]);
+    push(points[2]);
+
+    for (int i = 3; i < n; i++)
+    {
+        while (orientation(nextToTop(), stack[top], points[i]) != 2)
+            pop();
+        push(points[i]);
+    }
+
+    printf("Convex Hull points:\n");
+    for (int i = 0; i <= top; i++)
+        printf("(%d, %d)\n", stack[i].x, stack[i].y);
 }
 int main()
 {
@@ -35,5 +103,5 @@ int main()
 
     int n = sizeof(points) / sizeof(points[0]);
 
-    printf("%d", points[0].y);
+    convexHull(points, n);
 }
